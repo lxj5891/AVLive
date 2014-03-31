889a6172ad0d7073d1ab74dde442bb44979741ec
h_li <h_li@dreamats.co.jp>
2014-03-31 10:22:03 +0800

    new  Xcode 5.1 project

---
+++ AVLive/DAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DAViewController.h
+//  AVLive
+//
+//  Created by Antony on 14-3-31.
+//  Copyright (c) 2014年 DAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAViewController : UIViewController
+
+@end
---
+++ AVLive/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AVLive
+//
+//  Created by Antony on 14-3-31.
+//  Copyright (c) 2014年 DAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAppDelegate class]));
+    }
+}
